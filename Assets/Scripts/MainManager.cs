c7c25b98259109683ee7453829e5556adf8324a1
Alexander Hartmann <alex.hart.278@gmail.com>
2022-10-03 17:05:33 +0200

    init
    after the launch of Unity 3D 2021

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -18,10 +18,14 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
+
+
     
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
